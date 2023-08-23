202683b5e140ab2e74476e8122e4e4e2c99224b9
Pranav Anbarasu <pranavanba@gmail.com>
2023-08-23 15:56:28 -0700

    Accept command-line arguments

--- send_slack_message.py
+++ send_slack_message.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sys
 
 def send_slack_message(payload, webhook):
     """Send a Slack message to a channel via a webhook. 
@@ -13,3 +14,19 @@ def send_slack_message(payload, webhook):
     """
 
     return requests.post(webhook, json.dumps(payload))
+
+# Check if the script is being run with the correct number of arguments
+if len(sys.argv) != 3:
+    print("Usage: python send_slack.py <payload_json> <webhook_url>")
+    sys.exit(1)
+
+# Parse command-line arguments
+payload_json = sys.argv[1]
+webhook_url = sys.argv[2]
+
+# Convert the JSON string argument to a Python dictionary
+payload = json.loads(payload_json)
+
+# Call the send_slack_message function with the provided arguments
+response = send_slack_message(payload, webhook_url)
+print("HTTP response:", response)
